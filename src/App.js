192c78c91b553ce86aceff245183d07a3aa62115
RamaKrishnaPilla <ramakrishna.pilla@syndicationpro.com>
2022-09-11 17:49:57 +0530

    Latest Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCoffee,
+  faPeopleGroup,
+  faPersonCircleXmark,
+  faPersonBurst,
+  faCircleQuestion,
+} from "@fortawesome/free-solid-svg-icons";
+
 const App = () => {
   const getInitialState = () => {
     const value = "All";
@@ -34,24 +43,48 @@ const App = () => {
       });
       for (var col_i in filter_value_dict) {
         let filter_value = filter_value_dict[col_i];
-        let row_cell_value = col_cell_value_dict[col_i];
+        const linCodeValues1 = new Map();
+        linCodeValues1
+          .set("Blue", "BL")
+          .set("RED", "RD")
+          .set("Orange", "OR")
+          .set("Silver", "SV")
+          .set("Yellow", "YL")
+          .set("Green", "GR")
+          .set("NoColor", "NoColor");
+        console.log(filter_value, col_cell_value_dict[col_i], "##############");
         if (
-          row_cell_value.indexOf(filter_value) == -1 &&
-          filter_value != "all"
+          ["BL", "RD", "OR", "SV", "YL", "GR", "NoColor"].includes(filter_value)
         ) {
-          display_row = false;
-          break;
+          var row_cell_value = linCodeValues1.get(col_cell_value_dict[col_i]);
+          console.log(row_cell_value, "^^^^^^^^^^^^^");
+        } else {
+          var row_cell_value = col_cell_value_dict[col_i];
         }
+
+        console.log(row_cell_value, filter_value, "*********************");
+        if (row_cell_value)
+          if (
+            row_cell_value.indexOf(filter_value) == -1 &&
+            filter_value != "all"
+          ) {
+            display_row = false;
+            console.log("Inside Break");
+            break;
+          }
       }
       if (display_row == true) {
+        console.log("inside True");
         row.style.display = "table-row";
       } else {
+        console.log("Inside falase");
         row.style.display = "none";
       }
     });
   };
   const [trainData, setTrainData] = useState([]);
   useEffect(() => {
+    console.log("i fire once");
     const url =
       "https://api.wmata.com/TrainPositions/TrainPositions?contentType=json&api_key=c556af80af504f42ab0c1906b84f17ad";
     const id = setInterval(() => {
@@ -71,12 +104,20 @@ const App = () => {
         timeZone: "America/Los_Angeles",
       });
       setTimeValue(pst);
-    }, 5000);
+    }, 10000);
   }, []);
 
   const uniqueSerivces = new Map();
   const uniqueLineCode = new Map();
   const uniqueCarCount = new Map();
+  const linCodeValues = new Map();
+  linCodeValues
+    .set("RD", "Red")
+    .set("OR", "Orange")
+    .set("SV", "Silver")
+    .set("BL", "Blue")
+    .set("YL", "Yellow")
+    .set("GR", "Green");
 
   for (let i = 0; i < trainData.length; i++) {
     if (!uniqueSerivces.has(trainData[i].ServiceType)) {
@@ -95,6 +136,17 @@ const App = () => {
   lineData.unshift("all");
   const carCountData = [...uniqueCarCount.keys()];
   carCountData.unshift("all");
+
+  const serviceTyepIcons = new Map();
+  serviceTyepIcons.set("NoPassengers", faPersonCircleXmark);
+  serviceTyepIcons.set("Normal", faPeopleGroup);
+  serviceTyepIcons.set("Special", faPersonBurst);
+  serviceTyepIcons.set("Unknown", faCircleQuestion);
+  console.log(
+    serviceTyepIcons.get("NoPassengers"),
+    "*******************************"
+  );
+  console.log(trainData);
   return (
     <>
       {" "}
@@ -122,17 +174,19 @@ const App = () => {
                   </select>
                 </th>
                 <th col-index={4}>SecondsAtLocation</th>
-                <th col-index={5}>
-                  LineCode
-                  <select
-                    className="table-filter"
-                    onChange={(e) => handleChange(e)}
-                  >
-                    {lineData.map((value2) => {
-                      return <option value={value2}>{value2}</option>;
-                    })}
-                  </select>
-                </th>
+                <span>
+                  <th col-index={5}>
+                    LineCode
+                    <select
+                      className="table-filter"
+                      onChange={(e) => handleChange(e)}
+                    >
+                      {lineData.map((value2) => {
+                        return <option value={value2}>{value2}</option>;
+                      })}
+                    </select>
+                  </th>
+                </span>
                 <th col-index={6}>DirectionNum</th>
                 <th col-index={7}>DestinationStationCode</th>
                 <th col-index={8}>CircuitId</th>
@@ -155,9 +209,40 @@ const App = () => {
                   <tr>
                     <td>{train.TrainId}</td>
                     <td>{train.TrainNumber}</td>
-                    <td>{train.ServiceType}</td>
+                    <td>
+                      {train.ServiceType}{" "}
+                      <FontAwesomeIcon
+                        icon={serviceTyepIcons.get(train.ServiceType)}
+                      />
+                    </td>
                     <td>{train.SecondsAtLocation}</td>
-                    <td>{train.LineCode}</td>
+                    <td
+                      style={{
+                        height: "12px",
+                        width: "15px",
+                        borderColor: linCodeValues.get(train.LineCode),
+                        marginLeft: "5px",
+                        marginTop: "15px",
+                        borderRadius: "10px",
+                        borderWidth: "2px",
+                      }}
+                    >
+                      {train.LineCode === "RD"
+                        ? "RED"
+                        : train.LineCode === "OR"
+                        ? "Orange"
+                        : train.LineCode === "SV"
+                        ? "Silver"
+                        : train.LineCode === "BL"
+                        ? "Blue"
+                        : train.LineCode === "YL"
+                        ? "Yellow"
+                        : train.LineCode === "GR"
+                        ? "Green"
+                        : train.LineCode === null
+                        ? "No Color"
+                        : "Black"}
+                    </td>
                     <td>{train.DirectionNum}</td>
                     <td>{train.DestinationStationCode}</td>
                     <td>{train.CircuitId}</td>
